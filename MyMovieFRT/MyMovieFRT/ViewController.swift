6ec9f938ed564431c819c3c5297524d259c431a9
Archil Kuprashvili <archil@app3null.com>
2020-07-06 20:51:28 +0400

    Initial commit

---
+++ MyMovieFRT/MyMovieFRT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovieFRT
+//
+//  Created by Archil on 7/6/20.
+//  Copyright © 2020 Flat Rock Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
